f490fd7e073ac6ef59fdcc78245754251b5c96af
Sunny-jinn <rlawlsdn316@gmail.com>
2024-11-18 13:36:46 +0900

    feat: page indicator & full page css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,67 @@
-import './App.css'
+import { useRef, useState, useEffect } from 'react'
 import { FirstPage } from './components/FirstPage'
-import './styles/fonts.css'
 import { SecondPage } from './components/SecondPage'
+import { ThirdPage } from './components/ThirdPage'
+import { FourthPage } from './components/FourthPage'
+import ReactPageScroller from 'react-page-scroller'
+import { PageIndicator } from './components/PageIndicator'
+
+import './App.css'
+import './styles/fonts.css'
 
 function App() {
+  const [renderedPage, setRenderedPage] = useState(0)
+  const currentPageRef = useRef(0)
+  const indicatorRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (indicatorRef.current) {
+      if (currentPageRef.current >= 2) {
+        indicatorRef.current.style.display = 'block'
+      } else if (currentPageRef.current === 1) {
+        indicatorRef.current.style.display = 'none'
+      }
+    }
+  }, [renderedPage])
+
+  const handleBeforePageScroll = (nextPage: number) => {
+    currentPageRef.current = nextPage
+
+    if (indicatorRef.current) {
+      if (nextPage === 2) {
+        setTimeout(() => {
+          indicatorRef.current!.style.display = 'block'
+        }, 650)
+      } else if (nextPage === 1) {
+        setTimeout(() => {
+          indicatorRef.current!.style.display = 'none'
+        }, 300)
+      }
+    }
+  }
+
+  const handlePageChange = (nextPage: number) => {
+    setRenderedPage(nextPage)
+  }
+
   return (
-    <>
-      <FirstPage />
-      <SecondPage />
-    </>
+    <div>
+      <div
+        ref={indicatorRef}
+        style={{ display: renderedPage === 2 ? 'block' : 'none' }}
+      >
+        <PageIndicator currentPage={renderedPage} />
+      </div>
+      <ReactPageScroller
+        onBeforePageScroll={handleBeforePageScroll}
+        pageOnChange={handlePageChange}
+      >
+        <FirstPage />
+        <SecondPage />
+        <ThirdPage />
+        <FourthPage />
+      </ReactPageScroller>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react-page-scroller'], // CommonJS 모듈 강제 최적화
+  },
+  define: {
+    global: 'window', // 브라우저 환경에서 global을 window로 대체
+  },
+  base: '/sori/',
 })
